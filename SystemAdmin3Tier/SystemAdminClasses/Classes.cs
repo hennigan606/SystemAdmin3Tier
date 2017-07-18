5c3c0e351cfca6d8612a326da5f91af15580dec7
hennigan606 <hennigan07@hotmail.com>
2017-07-18 22:44:01 +0100

    18/07/2017

--- SystemAdmin3Tier/SystemAdminClasses/Classes.cs
+++ SystemAdmin3Tier/SystemAdminClasses/Classes.cs
@@ -1,4 +1,8 @@
-﻿namespace SystemAdminClasses
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SystemAdminClasses
 {
 
     public class User
@@ -7,6 +11,7 @@
         {
         }
 
+        [Key]
         public int UserID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
@@ -18,12 +23,28 @@
 
 
 
+    public class UserAccessGroup
+    {
+        public UserAccessGroup()
+        {
+        }
+
+        [Key]
+        public int AccessGroupID { get; set; }
+        public string GroupName { get; set; }
+        public List<int> GroupMemberIDs { get; set; }
+
+    }
+
+
+
     public class ServiceRequest
     {
         public ServiceRequest()
         {
         }
 
+        [Key]
         public int RequestID { get; set; }
         public string UserFullName { get; set; }
         public string RequestType { get; set; }
---
+++ SystemAdmin3Tier/SystemAdminDataModel/SystemAdminContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+using SystemAdminClasses;
+
+namespace SystemAdminDataModel
+{
+    public class SystemAdminContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserAccessGroup> AccessGroups { get; set; }
+        public DbSet<ServiceRequest> ServiceRequests { get; set; }
+    }
+}
