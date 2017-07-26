ab104b8090d31e6962203bf00d19a9c4fc94d5e9
Michael Hennigan <hennigan07@hotmail.com>
2017-07-26 09:00:46 +0000

    26/07/17

--- SystemAdmin3Tier/SystemAdminClasses/ServiceRequestService.cs
+++ SystemAdmin3Tier/SystemAdminClasses/ServiceRequestService.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SystemAdmin_CRUD_Ops;
 
 namespace SystemAdminClasses
 {
-    class ServiceRequestService
+    //ServiceRequestTracker stores a list of all service requests and contains methods
+    //for manipulating those requests, including creating requests, assigning an admin
+    //operator to a request, providing info on the completion of a request and
+    //marking a request as complete
+    public class ServiceRequestService
     {
+        CRUD_Operations CRUD = new CRUD_Operations();
+
+        List<ServiceRequest> Requests = new List<ServiceRequest>();
+
+        public ServiceRequestService()
+        {
+            Requests = CRUD.GetAllServiceRequests();
+        }
+
+
+
+        //Adds a new request to the system
+        public void CreateRequest(string UserFullName, string RequestType,
+            string Details)
+        {
+            //Add the new request to the database
+            CRUD.InsertServiceRequest(UserFullName, RequestType, Details);
+            //Update the list of service requests in memory to include new
+            //request added to the database
+            Requests = CRUD.GetAllServiceRequests();
+        }
+
+
+
+        
     }
 }
