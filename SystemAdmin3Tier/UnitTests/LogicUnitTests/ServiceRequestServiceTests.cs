8c61f05550b74a2240146e32251ae74ff09feb48
Michael Hennigan <hennigan07@hotmail.com>
2017-11-22 12:13:55 +0000

    22/11/2017

--- SystemAdmin3Tier/UnitTests/LogicUnitTests/ServiceRequestServiceTests.cs
+++ SystemAdmin3Tier/UnitTests/LogicUnitTests/ServiceRequestServiceTests.cs
@@ -9,6 +9,45 @@ namespace UnitTests.LogicUnitTests
     [TestFixture]
     public class ServiceRequestServiceTests
     {
+        [Test]
+        public void CreateRequest_CallsInsertServiceRequest_ExactlyOnce_WhenCalled()
+        {
+            //Arrange
+            string TestUserName = "Joe Bloggs";
+            string TestRequestType = "Password Reset";
+            string TestDetails = "Forgot Password";
 
+            Mock<ICRUD_Operations> MockCRUD = new Mock<ICRUD_Operations>();
+
+            ServiceRequestService RqstService = new ServiceRequestService(MockCRUD.Object);
+
+            //Act
+            RqstService.CreateRequest(TestUserName, TestRequestType, TestDetails);
+
+            //Assert
+            MockCRUD.Verify(x => x.InsertServiceRequest(
+                It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+        }
+
+
+        [Test]
+        public void CreateRequest_CallsInsertServiceRequest_PassingTheInformationProvided_ExactlyOnce_WhenCalled()
+        {
+            //Arrange
+            string TestUserName = "Joe Bloggs";
+            string TestRequestType = "Password Reset";
+            string TestDetails = "Forgot Password";
+
+            Mock<ICRUD_Operations> MockCRUD = new Mock<ICRUD_Operations>();
+
+            ServiceRequestService RqstService = new ServiceRequestService(MockCRUD.Object);
+
+            //Act
+            RqstService.CreateRequest(TestUserName, TestRequestType, TestDetails);
+
+            //Assert
+            MockCRUD.Verify(x => x.InsertServiceRequest(
+                TestUserName, TestRequestType, TestDetails), Times.Once);
+        }
     }
 }
